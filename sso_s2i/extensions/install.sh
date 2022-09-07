668ac4c42d8b178d95f7044d09c934af394c0aab
Antonio Arjona <aarjona@redhat.com>
2022-09-07 15:51:16 +0200

    test assemble 07

--- sso_s2i/extensions/install.sh
+++ sso_s2i/extensions/install.sh
@@ -4,13 +4,15 @@ injected_dir=$1
 
 echo "[S2I install.sh] injected_dir: ${injected_dir}"
 echo "[S2I install.sh] ENV_FILES---: ${ENV_FILES}"
+
+echo "/usr/local/s2i/install-common.sh: line 54: getConfigurationMode: command not found"
+sed -i "s|source \${JBOSS_HOME}/bin/launch/openshift-common.sh|source \${JBOSS_HOME}/bin/launch/openshift-common.sh\nsource \${JBOSS_HOME}/bin/launch/launch-common.sh\n|g" /usr/local/s2i/install-common.sh
+
 source /usr/local/s2i/install-common.sh
 #######################
 cp ${injected_dir}/configuration/* ${JBOSS_HOME}/standalone/configuration/
 
 # Workaround Solved:
-echo "/usr/local/s2i/install-common.sh: line 54: getConfigurationMode: command not found"
-sed -i "s|source \${JBOSS_HOME}/bin/launch/openshift-common.sh|source \${JBOSS_HOME}/bin/launch/openshift-common.sh\nsource \${JBOSS_HOME}/bin/launch/launch-common.sh\n|g" /usr/local/s2i/install-common.sh
 ####################
 # Add Oracle JDBC Driver Module & Driver
 echo "Before install_modules"
