b7eafc696268904e430d3c975cbe3f2de4c8a0a4
Antonio Arjona <aarjona@redhat.com>
2022-09-07 15:59:37 +0200

    test assemble 09

--- sso_s2i/extensions/install.sh
+++ sso_s2i/extensions/install.sh
@@ -5,7 +5,7 @@ injected_dir=$1
 echo "[S2I install.sh] injected_dir: ${injected_dir}"
 echo "[S2I install.sh] ENV_FILES---: ${ENV_FILES}"
 
-echo "Workaround: /usr/local/s2i/install-common.sh: line 54: getConfigurationMode: command not found"
+echo "[S2I install.sh] Workaround: /usr/local/s2i/install-common.sh: line 54: getConfigurationMode: command not found"
 sed -i "s|source \${JBOSS_HOME}/bin/launch/openshift-common.sh|source \${JBOSS_HOME}/bin/launch/openshift-common.sh\nsource \${JBOSS_HOME}/bin/launch/launch-common.sh\n|g" /usr/local/s2i/install-common.sh
 
 source /usr/local/s2i/install-common.sh
@@ -15,10 +15,10 @@ cp ${injected_dir}/configuration/* ${JBOSS_HOME}/standalone/configuration/
 # Workaround Solved:
 ####################
 # Add Oracle JDBC Driver Module & Driver
-echo "Before install_modules"
+echo "[S2I install.sh] Before install_modules"
 install_modules ${injected_dir}/modules
 #
-echo "Before configure drivers"
+echo "[S2I install.sh] Before configure drivers"
 configure_drivers ${injected_dir}/configuration/oracle_driver.properties
 #######################
 #######################
